6dfd3c9428a738f657f865de1cfde728695c204b
Janae Anderson <janae.anderson.2022@student.lhschools.org>
2020-03-04 13:35:05 -0500

    Intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,25 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+-----------------------------+");
+        System.out.println("| Janae Anderson Gas Station  |");
+        System.out.println("| 0505 Royalty Drive          |");
+        System.out.println("| Pataskala, Ohio 43062       |");
+        System.out.println("| Date           Time         |");
+        System.out.println("| 15JAN2020      12:05 PM     |");
+        System.out.println("| Involve #      212445       |");
+        System.out.println("| Auth#                       |");
+        System.out.println("| ***********12               |");
+        System.out.println("| Pump        Product    $/G  |");
+        System.out.println("| 05          unleaded   2.05 |");
+        System.out.println("|Amount       Fuel Total      |");
+        System.out.println("| 20.50          42.03        |");
+        System.out.println("|                             |");
+        System.out.println("|          Thank You          |");
+        System.out.println("|  Janae Anderson Gas Station |");
+        System.out.println("+-----------------------------+");
+
+    }
+}
